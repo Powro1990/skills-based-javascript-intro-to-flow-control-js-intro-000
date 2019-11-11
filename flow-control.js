a2865ceabeb8dd6c287de5911c7a8132f7c8f769
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2019-11-11 09:26:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,18 @@
 function basicTeenager(age) {
-  if(age > 12){
+  if(age > 12  && age < 20){
     return "You are a teenager!";
   }
 }
 
 function teenager(age) {
- if (12< age <20) {
+ if (age > 12  && age < 20) {
     return "You are a teenager!";
-  } else 
+  }
 
 }
 
 function ageChecker(age) {
-if (age > 12 || age <20) {
+if (age > 12  && age < 20) {
     return "You are a teenager!";
   }
 }
